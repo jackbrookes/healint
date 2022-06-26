e466ecdae549016bf87b246b40df712d1fea3e21
Jack brookes <jbrookes187@gmail.com>
2022-06-26 18:36:51 +0100

    add to main

--- Scripts/00_main.R
+++ Scripts/00_main.R
@@ -18,3 +18,14 @@ source("Scripts/04_sleep_processing.R")
 source("Scripts/05_read_sleep.R")
 
 source("Scripts/06_migraine_processing.R")
+source("Scripts/07_sleep_migraine_processing.R")
+
+# graphs
+source("Scripts/08_sleep_graphs.R")
+source("Scripts/09_migraine_graphs.R")
+source("Scripts/10_sleep_migraine_graphs.R")
+source("Scripts/11_demographics_graphs.R")
+
+# output
+source("Scripts/12_summary_tables.R")
+source("Scripts/13_statistics.R")
