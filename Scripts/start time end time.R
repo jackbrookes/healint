d9abda831bbfd07caf10b8186a3c8ae413d7da84
Jack brookes <jbrookes187@gmail.com>
2022-06-26 18:37:31 +0100

    delete old files

--- Scripts/start time end time.R
+++
@@ -1,7 +0,0 @@
-ggplot(region_data %>% filter(country == "GB"), aes(x = hour(sleep_starttime_local), color = country)) + 
-  geom_density(bw = 0.5)
-
-ggplot(region_data, aes(x = hour(sleep_endtime_local), color = country)) + 
-  geom_density(bw = 1)
-
-region_data <- left_join(sleep_data, demographic_data, by = "hashed_userid")
